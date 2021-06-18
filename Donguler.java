7109d06f6b3e7378b60c3c4fa374ba4853a665c7
Murathoca <m.murathoca@gmail.com>
2021-06-18 19:45:40 +0100

    deneme2

---
+++ Donguler.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package donguler;
+
+/**
+ *
+ * @author Murat
+ */
+public class Donguler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int sayi=0;
+       while (sayi<=20){
+           System.out.println(sayi);
+           sayi= sayi+2;
+       }
+        // TODO code application logic here
+    }
+    
+}
